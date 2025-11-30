bc9c2780a4fa1811299f657a6eb42e748f47c0b4
Kissy030 <807322562@qq.com>
2025-11-30 21:21:11 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
   });
-  await app.listen(3001);
+  await app.listen(3001, '0.0.0.0');
 }
 bootstrap();
