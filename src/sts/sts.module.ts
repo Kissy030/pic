a7a2b2365bccb05b181dda4e3457cd547da9601c
Kissy030 <807322562@qq.com>
2025-12-03 04:51:42 +0800

    fix

--- src/sts/sts.module.ts
+++ src/sts/sts.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StsService } from './sts.service';
+import { StsController } from './sts.controller';
 
 @Module({
   providers: [StsService],
+  controllers: [StsController],
   exports: [StsService],
 })
 export class StsModule {}
